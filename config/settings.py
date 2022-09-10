57c6b7c5240a9fd114d7b8ebe0e4f494c441d6ae
Ahmad Umaev <ahmad.um.2001@mail.ru>
2022-09-10 17:01:13 +0300

    Added some variables into .env

--- config/settings.py
+++ config/settings.py
@@ -229,9 +229,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 # EMAIL_HOST_USER = 'userdstu@gmail.com'
-EMAIL_HOST_USER = 'abulaysovv@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 # EMAIL_HOST_PASSWORD = 'EmhLRmpvMMZF47w'
-EMAIL_HOST_PASSWORD = 'sgvsgczdmjbvbvbl'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
