8a661bab1c56f691889d7aff0938f920d93bcb49
abulaysov <abulaysovv@mail.ru>
2022-10-16 23:44:23 +0300

    fixed and imporoved

--- users/models.py
+++ users/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-
     username = models.CharField(
         max_length=15,
         unique=True,
@@ -23,6 +22,8 @@ class CustomUser(AbstractUser):
                               })
 
     verified = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.username
