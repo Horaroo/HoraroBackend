862c19d071146b55a72cc9fcba01128ea490ebc8
abulaysov <abulaysovv@mail.ru>
2023-04-07 19:55:08 +0300

    hotfix: change week name

--- bots/services/constants.py
+++ bots/services/constants.py
@@ -10,3 +10,11 @@ BUTTONS = (
 
 CMD = ("/quickstart", "/add", "/del", "/help", "/token", "/pin", "/unpin", "/profile")
 CMD_MODER = ("/update",)
+DAYS_RU = {
+    "monday": "Понедельник",
+    "tuesday": "Вторник",
+    "wednesday": "Среда",
+    "thursday": "Четверг",
+    "friday": "Пятница",
+    "saturday": "Суббота",
+}
