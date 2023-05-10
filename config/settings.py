b22d1560060dafbacd84cec4279abdedcf709f70
abulaysov <abulaysovv@mail.ru>
2023-05-10 09:49:29 +0300

    hide swagger

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG"))
+# DEBUG = bool(os.getenv("DEBUG"))
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = [
     "https://horaro.net",
