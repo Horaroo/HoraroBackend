ff7e866670c9c1ecd938b5bf8bd80fbdf064d81e
abulaysov <abulaysovv@mail.ru>
2023-05-17 22:03:03 +0300

    removed extra code

--- bots/services/mixins/telegram_callback.py
+++ bots/services/mixins/telegram_callback.py
@@ -344,55 +344,6 @@ class TelegramCallbackSettings(BaseMixin):
         if data.startswith("M"):  # M - Menu
             return self._handle_callback_for_menu(callback_data)
         return self._handle_callback_for_setting(callback_data)
-        #
-        # if callback_data.call_data.startswith("M"):
-        #     return self._handle_callback_for_menu(callback_data)
-        # if callback_data.call_data == "menu":
-        #     return self.get_settings()
-        # elif callback_data.call_data == "help":
-        #     return self._get_help_data()
-        # elif callback_data.call_data in ("tokens", "menu-tokens"):
-        #     return self._get_tokens_data()
-        # elif callback_data.call_data == "quickstart":
-        #     return self._get_quickstart_data()
-        #
-        # elif callback_data.call_data == "add":
-        #     return self._get_add_data()
-        #
-        # elif callback_data.call_data in ("favorites", "menu-favorites"):
-        #     return self._get_favorites_data(callback_data, call_data="about-token-self")
-        #
-        # elif callback_data.call_data in ("pin", "menu-pin"):  # Choice group
-        #     return self._get_tokens_for_notification_data(callback_data)
-        # elif callback_data.call_data.startswith("menu-time"):  # Choice group
-        #     return self._get_data_time_menu(callback_data)
-        #
-        # elif callback_data.call_data.startswith("confirm-not"):  # Confirm notification
-        #     return self._get_confirm_notification_data(callback_data)
-        # elif re.search("(minus|plus)", callback_data.call_data):  # Choice time
-        #     return self._change_time_for_notification_data(callback_data)
-        # elif "pin-time" in callback_data.call_data:  # Choice action
-        #     return self._get_action_for_notification_data(callback_data)
-        # elif "pin-token" in callback_data.call_data:  # Choice token
-        #     return self._get_time_for_notification_data(callback_data)
-        #
-        # elif callback_data.call_data == "unpin":
-        #     return self._get_notification_data(callback_data)
-        # elif callback_data.call_data == "confirm-delete":
-        #     return self._get_confirm_delete_notification_data(callback_data)
-        #
-        # elif callback_data.call_data.startswith("about-token"):  # TODO: done
-        #     return self._get_about_token_data(callback_data)
-        #
-        # elif re.match(r"(del-token|add-token)", callback_data.call_data):  # TODO: done
-        #     if callback_data.call_data.startswith("del"):
-        #         self._delete_token(callback_data)
-        #     else:
-        #         self._add_token(callback_data)
-        #
-        #     if "self" in callback_data.call_data:
-        #         return self._get_about_token_data(callback_data, favorites_token=True)
-        #     return self._get_about_token_data(callback_data)
 
     def _handle_callback_for_setting(self, callback_data):
         """The order of conditions are important"""
